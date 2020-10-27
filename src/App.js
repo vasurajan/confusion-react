3a02f2dbff639d9dad86e0801ec7b9817c69fa05
vasurajan <63305945+vasurajan@users.noreply.github.com>
2020-10-27 18:19:35 +0530

    adding menu component and understanding lifting of state

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React from 'react'
 
 import { Navbar, NavbarBrand } from 'reactstrap';
+import { DISHES } from './shared/dishes';
+import Menu from './components/MenuComponent';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-        <NavbarBrand href="/">Phleebs Confusion</NavbarBrand>
-        </div>
-      </Navbar>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      dishes: DISHES
+    };
+  }
+  render() {
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Phleebs Confusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} />
+      </div>
+    )
+  }
 }
 
 export default App;
